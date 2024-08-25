cde6434cca5bf41ed4cd5a51b2060c0d768e1c61
Tanishk Yadav <studioustanishk@gmail.com>
2024-08-25 14:35:20 +0530

    fixed logo placement and footer

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@ function toggleCertificate(certId) {
         certElement.style.display = "none";
     }
 }
+
+function clickCertificate(certId){
+    if(certId=="cert1"||certId=="cert2"){
+        toggleCertificate("cert1");
+        toggleCertificate("cert2");
+    }
+    else if(certId=="cert3"||certId=="cert4"){
+        toggleCertificate("cert3");
+        toggleCertificate("cert4");
+    }
+    else if(certId=="cert5"||certId=="cert6"){
+        toggleCertificate("cert5");
+        toggleCertificate("cert6");
+    }
+    else if(certId=="cert7"||certId=="cert8"){
+        toggleCertificate("cert7");
+        toggleCertificate("cert8");
+    }
+}
